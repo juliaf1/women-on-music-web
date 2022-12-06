f55d0fba2eaa31b6f89880e2295ec3336efaffba
Julia Frederico <magalhaesjulia@id.uff.br>
2022-12-06 17:16:56 -0300

    feat: update start playlist button icon and text spacing

--- src/components/StartPlaylistButton/index.tsx
+++ src/components/StartPlaylistButton/index.tsx
@@ -7,7 +7,7 @@ export function StartPlaylistButton() {
     <button
       className={styles.startPlaylistButton}
     >
-      <FcMusic color="#eba417" className={styles.github} />
+      <FcMusic color="#eba417" className={styles.icon} />
       Gerar Playlist
     </button>
   )
