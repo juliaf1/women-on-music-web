a29cd80332591d7dbe376c13c515407219684b76
Julia Frederico <magalhaesjulia@id.uff.br>
2022-12-07 19:15:22 -0300

    chore: remove unused session components from app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,26 +1,18 @@
 import { AppProps } from 'next/app';
-import { Session } from 'next-auth';
-import { SessionProvider } from 'next-auth/react';
 
 import { Header } from '../components/Header';
 
 import '../styles/global.scss';
 
-interface MyAppProps extends AppProps {
-  pageProps: {
-    session: Session;
-  }
-}
-
 function MyApp({
   Component,
-  pageProps: { session, ...pageProps }
-}: MyAppProps) {
+  pageProps: { ...pageProps }
+}: AppProps) {
   return(
-    <SessionProvider session={session}>
+    <>
       <Header />
       <Component {...pageProps} />
-    </SessionProvider>
+    </>
   )
 }
 
