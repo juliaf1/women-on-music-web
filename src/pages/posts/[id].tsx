e5fd4c5d45be24c60f5223e0b2c66e58a8b8fbb3
Julia Frederico <magalhaesjulia@id.uff.br>
2022-12-07 16:00:27 -0300

    chore: clean up unused functionalities in post page

--- src/pages/posts/[id].tsx
+++ src/pages/posts/[id].tsx
@@ -1,5 +1,4 @@
 import { GetServerSideProps } from 'next';
-import { getSession } from 'next-auth/react';
 import Head from 'next/head';
 
 import styles from './post.module.scss';
@@ -35,24 +34,12 @@ export default function Post({ post }: PostProps) {
 };
 
 export const getServerSideProps: GetServerSideProps = async ({ req, params }) => {
-  const session = await getSession({ req });
-  const { id } = params;
-
-  if (!session?.activeSubscription) {
-    return {
-      redirect: {
-        destination: `/posts/preview/${id}`,
-        permanent: false,
-      }
-    };
-  };
-
   // Request API/DB for post with that slug/id
 
   return {
     props: {
       post: {
-        id,
+        id: 0,
         title: 'Creating a Next application with TS',
         content: '<p>In this guide, you will learn how to create a <a href="/">Next</a> application with TypeScript from scratch... Next.js is an open-source web development framework created by Vercel enabling React-based web applications with server-side rendering and generating static websites.</p>',
         created_at: 'Sunday, October 02, 2022',
