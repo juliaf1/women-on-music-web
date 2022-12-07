9af5b6c5fdfcae02690b79b28e3bd86d9c6bcb06
Julia Frederico <magalhaesjulia@id.uff.br>
2022-12-07 18:34:22 -0300

    feat: update artist content interface + starts adding content stub

--- src/utils/artists.ts
+++ src/utils/artists.ts
@@ -3,10 +3,11 @@ export interface Artist {
   name: string;
   briefing: string;
   city: string;
-  video_url: string;
   image_url: string;
-  title: string;
-  content: string;
+  content: {
+    main_video_url: string;
+    videos: Array<Video>;
+  };
   socials: {
     spotify: string;
     website: string;
@@ -14,20 +15,59 @@ export interface Artist {
   }
 };
 
-export const artists: Array<Artist> = [
-  {
-    id: "joyce",
-    name: "joyce",
-    briefing: "joyce",
-    city: "joyce",
-    video_url: "joyce",
-    image_url: "joyce",
-    title: "joyce",
-    content: "joyce",
-    socials: {
-      spotify: "joyce",
-      website: "joyce",
-      instagram: "joyce",
-    }
+interface Video {
+  video_url: string;
+  title: string;
+  description: string;
+}
+
+const artist_1: Artist = {
+  id: "joyce",
+  name: "Joyce Moreno",
+  briefing: "Nascida no Rio de Janeiro, a cantora, compositora, arranjadora e instrumentista, Joyce Moreno já lançou 34 CDs e 2 DVDs individuais, além de compilações, participações e cerca de 400 músicas gravadas por alguns dos maiores nomes da música popular brasileira, como Elis Regina, Maria Bethania, Monica Salmaso, Gal Costa, Milton Nascimento, Ney Matogrosso, Edu Lobo, Emilio Santiago, Boca Livre, Nana Caymmi, Zizi Possi, Elizeth Cardoso, Simone, Leny Andrade, entre muitos outros.",
+  city: "Rio de Janeiro",
+  image_url: "joyce",
+  content: {
+    main_video_url: "",
+    videos: [
+      {
+        video_url: "",
+        title: "",
+        description: "",
+      }
+    ],
   },
+  socials: {
+    spotify: "joyce",
+    website: "joyce",
+    instagram: "joyce",
+  }
+}
+
+const artist_2: Artist = {
+  id: "joyce",
+  name: "Joyce Moreno",
+  briefing: "Nascida no Rio de Janeiro, a cantora, compositora, arranjadora e instrumentista, Joyce Moreno já lançou 34 CDs e 2 DVDs individuais, além de compilações, participações e cerca de 400 músicas gravadas por alguns dos maiores nomes da música popular brasileira, como Elis Regina, Maria Bethania, Monica Salmaso, Gal Costa, Milton Nascimento, Ney Matogrosso, Edu Lobo, Emilio Santiago, Boca Livre, Nana Caymmi, Zizi Possi, Elizeth Cardoso, Simone, Leny Andrade, entre muitos outros.",
+  city: "Rio de Janeiro",
+  image_url: "joyce",
+  content: {
+    main_video_url: "",
+    videos: [
+      {
+        video_url: "",
+        title: "",
+        description: "",
+      }
+    ],
+  },
+  socials: {
+    spotify: "joyce",
+    website: "joyce",
+    instagram: "joyce",
+  }
+}
+
+export const artists: Array<Artist> = [
+  artist_1,
+  artist_2
 ];
