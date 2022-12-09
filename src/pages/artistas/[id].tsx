3bcd9c916f847132b23f790f7e0ca7b479ff42a5
Julia Frederico <magalhaesjulia@id.uff.br>
2022-12-09 17:15:21 -0300

    feat: add degrade to artist page

--- src/pages/artistas/[id].tsx
+++ src/pages/artistas/[id].tsx
@@ -19,6 +19,8 @@ export default function Artist({ artist }: Props) {
         <title>{`${artist.name} | Música Brasileira` }</title>
       </Head>
 
+      <div className={styles.degradeBackground} />
+
       <div className={styles.container}>
         <ArtistOverview artist={artist}/>
         <ArtistDetail artist={artist} />
