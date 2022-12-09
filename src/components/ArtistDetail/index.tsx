1566d4fed345b8dee7505c4074991440f7e80243
Julia Frederico <magalhaesjulia@id.uff.br>
2022-12-09 14:38:02 -0300

    feat: add and style back to playlist button

--- src/components/ArtistDetail/index.tsx
+++ src/components/ArtistDetail/index.tsx
@@ -1,15 +1,19 @@
+import { IoMdArrowBack } from 'react-icons/io';
+
 import { Artist as ArtistProps } from '../../utils/artists';
 
 import styles from './styles.module.scss';
 
 export function ArtistDetail({ artist }: ArtistProps) {
   return(
-    <div>
+    <div className={styles.container}>
+          <a href="https://www.ubc.org.br/anexos/publicacoes/Por-Elas-Que-Fazem-a-Musica-2021.pdf" target="blank" className={styles.link}>
+            <IoMdArrowBack size={18} className={styles.icon} />
+            Voltar para playlist
+          </a>
 
-
-      <h1>{artist.name}</h1>
       <img src={artist.image_url} alt="" />
-
+      <h1>{artist.name}</h1>
     </div>
   )
 };
