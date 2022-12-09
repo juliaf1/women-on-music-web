01be0f8ce74f42ad49d039c33d5ac0d49ea3a31e
Julia Frederico <magalhaesjulia@id.uff.br>
2022-12-09 17:02:45 -0300

    feat: update artist overview layout

--- src/components/ArtistOverview/index.tsx
+++ src/components/ArtistOverview/index.tsx
@@ -1,4 +1,6 @@
 import { IoMdArrowBack } from 'react-icons/io';
+import { BsSpotify, BsInstagram } from 'react-icons/bs';
+import { BsLink45Deg } from 'react-icons/bs';
 
 import { Artist as ArtistProps } from '../../utils/artists';
 
@@ -11,14 +13,29 @@ interface Props {
 export function ArtistOverview({ artist }: Props) {
   return(
     <div className={styles.container}>
-      <a href="/artistas" className={styles.link}>
+      <a href="/artistas" className={styles.backLink}>
         <IoMdArrowBack size={18} className={styles.icon} />
         Voltar para playlist
       </a>
 
       <div className={styles.contentContainer}>
-        <img src={artist.image_url} alt="" />
+        <img src={artist.image_url} />
         <h1>{artist.name}</h1>
+        <span>{artist.city}</span>
+
+        <div className={styles.socials}>
+          <a href={artist.socials.spotify} className={styles.link} target="blank">
+            <BsSpotify size={18} className={styles.icon} />
+          </a>
+
+          <a href={artist.socials.instagram} className={styles.link} target="blank">
+            <BsInstagram size={18} className={styles.icon} />
+          </a>
+
+          <a href={artist.socials.website} className={styles.link} target="blank">
+            <BsLink45Deg size={18} className={styles.icon} />
+          </a>
+        </div>
       </div>
     </div>
   )
