289a2ef0aa649da9188b0947c1034c1695490091
Julia Frederico <magalhaesjulia@id.uff.br>
2022-12-09 21:16:04 -0300

    feat: add all mvp arists with correct images

--- src/utils/artists.ts
+++ src/utils/artists.ts
@@ -21,7 +21,7 @@ interface Video {
   description: string;
 }
 
-const artist_1: Artist = {
+const ellen: Artist = {
   id: "ellen",
   name: "Ellen Oléria",
   briefing: "Nascida no Rio de Janeiro, a cantora, compositora, arranjadora e instrumentista, Joyce Moreno já lançou 34 CDs e 2 DVDs individuais, além de compilações, participações e cerca de 400 músicas gravadas por alguns dos maiores nomes da música popular brasileira, como Elis Regina, Maria Bethania, Monica Salmaso, Gal Costa, Milton Nascimento, Ney Matogrosso, Edu Lobo, Emilio Santiago, Boca Livre, Nana Caymmi, Zizi Possi, Elizeth Cardoso, Simone, Leny Andrade, entre muitos outros.",
@@ -44,7 +44,7 @@ const artist_1: Artist = {
   }
 }
 
-const artist_2: Artist = {
+const joyce: Artist = {
   id: "joyce",
   name: "Joyce Moreno",
   briefing: "Nascida no Rio de Janeiro, a cantora, compositora, arranjadora e instrumentista, Joyce Moreno já lançou 34 CDs e 2 DVDs individuais, além de compilações, participações e cerca de 400 músicas gravadas por alguns dos maiores nomes da música popular brasileira, como Elis Regina, Maria Bethania, Monica Salmaso, Gal Costa, Milton Nascimento, Ney Matogrosso, Edu Lobo, Emilio Santiago, Boca Livre, Nana Caymmi, Zizi Possi, Elizeth Cardoso, Simone, Leny Andrade, entre muitos outros.",
@@ -67,7 +67,151 @@ const artist_2: Artist = {
   }
 }
 
+const indiana: Artist = {
+  id: "indiana",
+  name: "Indiana Nomma",
+  briefing: "Nascida no Rio de Janeiro, a cantora, compositora, arranjadora e instrumentista, Joyce Moreno já lançou 34 CDs e 2 DVDs individuais, além de compilações, participações e cerca de 400 músicas gravadas por alguns dos maiores nomes da música popular brasileira, como Elis Regina, Maria Bethania, Monica Salmaso, Gal Costa, Milton Nascimento, Ney Matogrosso, Edu Lobo, Emilio Santiago, Boca Livre, Nana Caymmi, Zizi Possi, Elizeth Cardoso, Simone, Leny Andrade, entre muitos outros.",
+  city: "Algum Lugar",
+  image_url: "https://res.cloudinary.com/dz2czxltr/image/upload/v1670630993/TCC/indiana_bpzrx8.png",
+  content: {
+    main_video_url: "https://www.youtube.com/embed/Uc-yb_IfpEU",
+    videos: [
+      {
+        video_url: "https://www.youtube.com/embed/uhIhBu_ajW4",
+        title: "Referências negras na música brasileira",
+        description: "Nascida no Rio de Janeiro, a cantora, compositora, arranjadora e instrumentista, Joyce Moreno já lançou 34 CDs e 2 DVDs individuais, além de compilações, participações e cerca de 400 músicas gravadas por alguns dos maiores nomes da música popular brasileira, como Elis Regina, Maria Bethania, Monica Salmaso, Gal Costa, Milton Nascimento, Ney Matogrosso, Edu Lobo, Emilio Santiago, Boca Livre, Nana Caymmi, Zizi Possi, Elizeth Cardoso, Simone, Leny Andrade, entre muitos outros.",
+      }
+    ],
+  },
+  socials: {
+    spotify: "https://open.spotify.com/artist/2Lw4xNNXMrVhYWLNTmBo7B",
+    website: "https://www.ellenoleria.com.br/",
+    instagram: "https://www.instagram.com/ellenoleria/?hl=en",
+  }
+}
+
+const vanessa: Artist = {
+  id: "vanessa",
+  name: "Vanessa Moreno",
+  briefing: "Nascida no Rio de Janeiro, a cantora, compositora, arranjadora e instrumentista, Joyce Moreno já lançou 34 CDs e 2 DVDs individuais, além de compilações, participações e cerca de 400 músicas gravadas por alguns dos maiores nomes da música popular brasileira, como Elis Regina, Maria Bethania, Monica Salmaso, Gal Costa, Milton Nascimento, Ney Matogrosso, Edu Lobo, Emilio Santiago, Boca Livre, Nana Caymmi, Zizi Possi, Elizeth Cardoso, Simone, Leny Andrade, entre muitos outros.",
+  city: "Algum Lugar",
+  image_url: "https://res.cloudinary.com/dz2czxltr/image/upload/v1670630993/TCC/vanessa_xb8vzc.png",
+  content: {
+    main_video_url: "https://www.youtube.com/embed/Uc-yb_IfpEU",
+    videos: [
+      {
+        video_url: "https://www.youtube.com/embed/uhIhBu_ajW4",
+        title: "Referências negras na música brasileira",
+        description: "Nascida no Rio de Janeiro, a cantora, compositora, arranjadora e instrumentista, Joyce Moreno já lançou 34 CDs e 2 DVDs individuais, além de compilações, participações e cerca de 400 músicas gravadas por alguns dos maiores nomes da música popular brasileira, como Elis Regina, Maria Bethania, Monica Salmaso, Gal Costa, Milton Nascimento, Ney Matogrosso, Edu Lobo, Emilio Santiago, Boca Livre, Nana Caymmi, Zizi Possi, Elizeth Cardoso, Simone, Leny Andrade, entre muitos outros.",
+      }
+    ],
+  },
+  socials: {
+    spotify: "https://open.spotify.com/artist/2Lw4xNNXMrVhYWLNTmBo7B",
+    website: "https://www.ellenoleria.com.br/",
+    instagram: "https://www.instagram.com/ellenoleria/?hl=en",
+  }
+}
+
+const sandra: Artist = {
+  id: "sandra",
+  name: "Sandra de Sá",
+  briefing: "Nascida no Rio de Janeiro, a cantora, compositora, arranjadora e instrumentista, Joyce Moreno já lançou 34 CDs e 2 DVDs individuais, além de compilações, participações e cerca de 400 músicas gravadas por alguns dos maiores nomes da música popular brasileira, como Elis Regina, Maria Bethania, Monica Salmaso, Gal Costa, Milton Nascimento, Ney Matogrosso, Edu Lobo, Emilio Santiago, Boca Livre, Nana Caymmi, Zizi Possi, Elizeth Cardoso, Simone, Leny Andrade, entre muitos outros.",
+  city: "Algum Lugar",
+  image_url: "https://res.cloudinary.com/dz2czxltr/image/upload/v1670630993/TCC/sandra_it7lc9.png",
+  content: {
+    main_video_url: "https://www.youtube.com/embed/Uc-yb_IfpEU",
+    videos: [
+      {
+        video_url: "https://www.youtube.com/embed/uhIhBu_ajW4",
+        title: "Referências negras na música brasileira",
+        description: "Nascida no Rio de Janeiro, a cantora, compositora, arranjadora e instrumentista, Joyce Moreno já lançou 34 CDs e 2 DVDs individuais, além de compilações, participações e cerca de 400 músicas gravadas por alguns dos maiores nomes da música popular brasileira, como Elis Regina, Maria Bethania, Monica Salmaso, Gal Costa, Milton Nascimento, Ney Matogrosso, Edu Lobo, Emilio Santiago, Boca Livre, Nana Caymmi, Zizi Possi, Elizeth Cardoso, Simone, Leny Andrade, entre muitos outros.",
+      }
+    ],
+  },
+  socials: {
+    spotify: "https://open.spotify.com/artist/2Lw4xNNXMrVhYWLNTmBo7B",
+    website: "https://www.ellenoleria.com.br/",
+    instagram: "https://www.instagram.com/ellenoleria/?hl=en",
+  }
+}
+
+const evinha: Artist = {
+  id: "evinha",
+  name: "Evinha",
+  briefing: "Nascida no Rio de Janeiro, a cantora, compositora, arranjadora e instrumentista, Joyce Moreno já lançou 34 CDs e 2 DVDs individuais, além de compilações, participações e cerca de 400 músicas gravadas por alguns dos maiores nomes da música popular brasileira, como Elis Regina, Maria Bethania, Monica Salmaso, Gal Costa, Milton Nascimento, Ney Matogrosso, Edu Lobo, Emilio Santiago, Boca Livre, Nana Caymmi, Zizi Possi, Elizeth Cardoso, Simone, Leny Andrade, entre muitos outros.",
+  city: "Algum Lugar",
+  image_url: "https://res.cloudinary.com/dz2czxltr/image/upload/v1670630993/TCC/evinha_g2tuom.png",
+  content: {
+    main_video_url: "https://www.youtube.com/embed/Uc-yb_IfpEU",
+    videos: [
+      {
+        video_url: "https://www.youtube.com/embed/uhIhBu_ajW4",
+        title: "Referências negras na música brasileira",
+        description: "Nascida no Rio de Janeiro, a cantora, compositora, arranjadora e instrumentista, Joyce Moreno já lançou 34 CDs e 2 DVDs individuais, além de compilações, participações e cerca de 400 músicas gravadas por alguns dos maiores nomes da música popular brasileira, como Elis Regina, Maria Bethania, Monica Salmaso, Gal Costa, Milton Nascimento, Ney Matogrosso, Edu Lobo, Emilio Santiago, Boca Livre, Nana Caymmi, Zizi Possi, Elizeth Cardoso, Simone, Leny Andrade, entre muitos outros.",
+      }
+    ],
+  },
+  socials: {
+    spotify: "https://open.spotify.com/artist/2Lw4xNNXMrVhYWLNTmBo7B",
+    website: "https://www.ellenoleria.com.br/",
+    instagram: "https://www.instagram.com/ellenoleria/?hl=en",
+  }
+}
+
+const leny: Artist = {
+  id: "leny",
+  name: "Leny Andrade",
+  briefing: "Nascida no Rio de Janeiro, a cantora, compositora, arranjadora e instrumentista, Joyce Moreno já lançou 34 CDs e 2 DVDs individuais, além de compilações, participações e cerca de 400 músicas gravadas por alguns dos maiores nomes da música popular brasileira, como Elis Regina, Maria Bethania, Monica Salmaso, Gal Costa, Milton Nascimento, Ney Matogrosso, Edu Lobo, Emilio Santiago, Boca Livre, Nana Caymmi, Zizi Possi, Elizeth Cardoso, Simone, Leny Andrade, entre muitos outros.",
+  city: "Algum Lugar",
+  image_url: "https://res.cloudinary.com/dz2czxltr/image/upload/v1670630994/TCC/leny_azlmtb.png",
+  content: {
+    main_video_url: "https://www.youtube.com/embed/Uc-yb_IfpEU",
+    videos: [
+      {
+        video_url: "https://www.youtube.com/embed/uhIhBu_ajW4",
+        title: "Referências negras na música brasileira",
+        description: "Nascida no Rio de Janeiro, a cantora, compositora, arranjadora e instrumentista, Joyce Moreno já lançou 34 CDs e 2 DVDs individuais, além de compilações, participações e cerca de 400 músicas gravadas por alguns dos maiores nomes da música popular brasileira, como Elis Regina, Maria Bethania, Monica Salmaso, Gal Costa, Milton Nascimento, Ney Matogrosso, Edu Lobo, Emilio Santiago, Boca Livre, Nana Caymmi, Zizi Possi, Elizeth Cardoso, Simone, Leny Andrade, entre muitos outros.",
+      }
+    ],
+  },
+  socials: {
+    spotify: "https://open.spotify.com/artist/2Lw4xNNXMrVhYWLNTmBo7B",
+    website: "https://www.ellenoleria.com.br/",
+    instagram: "https://www.instagram.com/ellenoleria/?hl=en",
+  }
+}
+
+const mariateresa: Artist = {
+  id: "maria-teresa",
+  name: "Maria Teresa Madeira",
+  briefing: "Nascida no Rio de Janeiro, a cantora, compositora, arranjadora e instrumentista, Joyce Moreno já lançou 34 CDs e 2 DVDs individuais, além de compilações, participações e cerca de 400 músicas gravadas por alguns dos maiores nomes da música popular brasileira, como Elis Regina, Maria Bethania, Monica Salmaso, Gal Costa, Milton Nascimento, Ney Matogrosso, Edu Lobo, Emilio Santiago, Boca Livre, Nana Caymmi, Zizi Possi, Elizeth Cardoso, Simone, Leny Andrade, entre muitos outros.",
+  city: "Algum Lugar",
+  image_url: "https://res.cloudinary.com/dz2czxltr/image/upload/v1670630993/TCC/mariateresa_emgbgk.png",
+  content: {
+    main_video_url: "https://www.youtube.com/embed/Uc-yb_IfpEU",
+    videos: [
+      {
+        video_url: "https://www.youtube.com/embed/uhIhBu_ajW4",
+        title: "Referências negras na música brasileira",
+        description: "Nascida no Rio de Janeiro, a cantora, compositora, arranjadora e instrumentista, Joyce Moreno já lançou 34 CDs e 2 DVDs individuais, além de compilações, participações e cerca de 400 músicas gravadas por alguns dos maiores nomes da música popular brasileira, como Elis Regina, Maria Bethania, Monica Salmaso, Gal Costa, Milton Nascimento, Ney Matogrosso, Edu Lobo, Emilio Santiago, Boca Livre, Nana Caymmi, Zizi Possi, Elizeth Cardoso, Simone, Leny Andrade, entre muitos outros.",
+      }
+    ],
+  },
+  socials: {
+    spotify: "https://open.spotify.com/artist/2Lw4xNNXMrVhYWLNTmBo7B",
+    website: "https://www.ellenoleria.com.br/",
+    instagram: "https://www.instagram.com/ellenoleria/?hl=en",
+  }
+}
+
 export const artists: Array<Artist> = [
-  artist_1,
-  artist_2,
+  ellen,
+  joyce,
+  indiana,
+  vanessa,
+  sandra,
+  evinha,
+  leny,
+  mariateresa
 ];
